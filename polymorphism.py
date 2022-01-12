bbb7bcdbf90715f417224a5466640e2e641afd0b
DennisCodeGuru <denniskalmykovv@gmail.com>
2022-01-11 17:33:43 -0800

    polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,45 @@
+
+
+
+#Parent Class User
+class User:
+    name = "Mark"
+    email = "mark@gmail.com"
+    password = "1234abcd"
+
+    def getLoginInfo(self):
+        entry_name = input("Enter your name: ")
+        entry_email = input("Enter your email: ")
+        entry_password = input("Enter your password: ")
+        if (entry_email == self.email and entry_password == self.password):
+            print("Welcome back, {}!".format(entry_name))
+        else:
+            print("The password or email si incorrect.")
+
+
+#Child Class Employee
+class Employee(User):
+    base_pay = 11.00
+    department = "General"
+    pin_number = "3980"
+
+
+#This is the same method in the parent class "User".
+# The difference is that, insted ofusing entry_password, we're using entry_pin.
+
+    def getLoginInfo(self):
+        entry_name = input("Enter your name: ")
+        entry_email = input("Enter your email: ")
+        entry_password = input("Enter your Pin: ")
+        if (entry_email == self.email and entry_pin == self.pin_number):
+            print("Welcome back, {}!".format(entry_name))
+        else:
+            print("The pin or email si incorrect.")
+
+#The folowing code invokes the methods inside each class for User and Employee.
+        
+customer = User()
+customer.getLoginInfo()
+
+manager = Employee()
+manager.getLoginInfo()
